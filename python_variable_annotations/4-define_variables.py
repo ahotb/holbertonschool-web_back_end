60fcade0cb5a524c6fec78df87411d463201cb70
ahotb <ah.r9625@gmail.com>
2026-04-12 14:02:28 +0300

    fixed

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -5,8 +5,10 @@ a: int = 1
 """Defines a variable pi with the value 3.14 and an annotation of float"""
 pi: float = 3.14
 
-"""Defines a variable i_understand_annotations with the value True and an annotation of bool"""
+"""Defines a variable i_understand_annotations
+ with the value True and an annotation of bool"""
 i_understand_annotations: bool = True
 
-"""Defines a variable school with the value Holberton and an annotation of str"""
+"""Defines a variable school with 
+the value Holberton and an annotation of str"""
 school: str = "Holberton"
