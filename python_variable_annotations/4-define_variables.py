d826130e58ca93442c7d1cbf2c14abb7c15cb8dc
ahotb <ah.r9625@gmail.com>
2026-04-12 14:01:32 +0300

    Refactor variable definitions to include type annotations and improve clarity

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
+"""Defines variables with annotations"""
+a: int = 1
 
-a = 1
+"""Defines a variable pi with the value 3.14 and an annotation of float"""
+pi: float = 3.14
 
+"""Defines a variable i_understand_annotations with the value True and an annotation of bool"""
+i_understand_annotations: bool = True
 
-pi = 3.14
-
-
-i_understand_annotations = True
-
-
-school = "Holberton"
+"""Defines a variable school with the value Holberton and an annotation of str"""
+school: str = "Holberton"
