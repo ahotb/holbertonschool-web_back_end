701c69f3a25216150569afcc7cd8454f888fd093
ahotb <ah.r9625@gmail.com>
2026-04-12 13:26:27 +0300

    fixed

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function concat that takes two strings str1 and str2 as arguments and returns a concatenated string.
+"""Write a type-annotated function concat that takes two strings
+ str1 and str2 as arguments and returns a concatenated string.
 
 """
 
