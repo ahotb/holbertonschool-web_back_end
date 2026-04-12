f77cc16366e194e821470c955bfd0b828cebc3dc
ahotb <ah.r9625@gmail.com>
2026-04-12 14:41:44 +0300

    fixed

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,5 +4,6 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """The input list is annotated as a list of floats, and the return value is annotated as a float."""
+    """The input list is annotated as a list of floats,
+      and the return value is annotated as a float."""
     return sum(input_list)
