f3c118ff33c453643ea3e67e7dbb173e3bb124eb
ahotb <ah.r9625@gmail.com>
2026-04-27 11:48:37 +0300

    fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-def index_range(page, page_size):
+from typing import Tuple
+
+
+def index_range(page, page_size) -> tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
     return (start,  end)
