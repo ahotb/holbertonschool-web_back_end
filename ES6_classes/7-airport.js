2ddda62e3485c99b5fba5924c2f0603097ac5d1e
ahotb <ah.r9625@gmail.com>
2026-05-05 21:12:18 +0300

    feat: add Airport class and main script for instantiation and logging

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,15 @@
+export default class Airport {
+	constructor(name, code) {
+		this._name = name;
+		this._code = code;
+	}
+	get name () {
+		return this._name;
+	}
+	get code () {
+		return this._code;
+	}
+	Tostring () {
+		return `[object ${this._code}]`;
+	}
+}
